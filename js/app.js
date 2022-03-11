bcb0cf64364a16a1e98348064c6e8987b9841823
Dev-Zual <freelancerzual@gmail.com>
2022-03-11 11:21:57 +0600

    solved liked post doubble append

--- js/app.js
+++ js/app.js
@@ -153,6 +153,7 @@ const showPosts = (posts) => {
 
 const displayLikedPosts = () => {
   const likedPosts = getLikedPosts();
+  document.getElementById('liked').innerHTML = '';
   likedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById('liked').appendChild(div);
