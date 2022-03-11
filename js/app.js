1ef6fdd7960dab8285321e85e94260534fb93958
Dev-Zual <freelancerzual@gmail.com>
2022-03-11 11:09:31 +0600

    solved report post double append

--- js/app.js
+++ js/app.js
@@ -161,6 +161,7 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
   const reportedPosts = getReportedPosts();
+  document.getElementById('reported').innerHTML = '';
   reportedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById('reported').appendChild(div);
