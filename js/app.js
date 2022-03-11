9716611484e8e97e15d4afda28d8c7f6d14bdd3d
Dev-Zual <freelancerzual@gmail.com>
2022-03-11 11:06:58 +0600

    solved reported post

--- js/app.js
+++ js/app.js
@@ -161,7 +161,7 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
   const reportedPosts = getReportedPosts();
-  posts.forEach((post) => {
+  reportedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById('reported').appendChild(div);
   });
